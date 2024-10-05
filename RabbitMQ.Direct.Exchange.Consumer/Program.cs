71644774e564b61e53d3242ac6712671f58f0273
nurullahnamal <nurullahnamal4@gmail.com>
2024-10-05 20:56:20 +0300

    direct exchange

---
+++ RabbitMQ.Direct.Exchange.Consumer/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQ.Direct.Exchange.Consumer
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			//Create Connection
+			ConnectionFactory factory = new();
+
+			factory.Uri = new("amqps://xhdvzihf:km496JVr3_W5KgaWbdxr9PKB3lJ2ULpD@shrimp.rmq." +
+				"cloudamqp.com/xhdvzihf");
+
+			// Connection Open 
+			using IConnection connection = factory.CreateConnection();
+
+			// Create Channel
+
+			using IModel channel = connection.CreateModel();
+
+			// create queue 
+			channel.ExchangeDeclare(exchange: "direct-exchange-example", type:ExchangeType.Direct);
+
+			// send message Queue
+
+			while (true) {
+
+                Console.WriteLine("Mesaj :");
+
+				string message =Console.ReadLine();
+				byte[] byteMessage=Encoding.UTF8.GetBytes(message);
+
+				channel.BasicPublish(
+					exchange: "direct-exchange-example",
+					routingKey: "direct-queue-example",
+					body: byteMessage);
+			}
+			Console.Read();
+		}
+	}
+}
---
+++ RabbitMQ.Direct.Exchange.Publisher/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQ.Direct.Exchange.Publisher
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Create Connection
+			ConnectionFactory factory = new();
+
+			factory.Uri = new("amqps://xhdvzihf:km496JVr3_W5KgaWbdxr9PKB3lJ2ULpD@shrimp.rmq." +
+				"cloudamqp.com/xhdvzihf");
+
+			// Connection Open 
+			using IConnection connection = factory.CreateConnection();
+
+			// Create Channel
+
+			using IModel channel = connection.CreateModel();
+
+			channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+			string queueName = channel.QueueDeclare().QueueName;
+
+
+			channel.QueueBind(queue: queueName,
+				exchange: "direct-exchange-example",
+				routingKey: "direct-queue-example");
+
+			EventingBasicConsumer consumer = new(channel);
+			channel.BasicConsume(queue: queueName, autoAck: true, consumer:consumer);
+
+			consumer.Received += (sender, e) =>
+			{
+				string message = Encoding.UTF8.GetString(e.Body.Span);
+				Console.WriteLine(message);
+			};
+
+			Console.Read();
+
+
+
+
+		}
+	}
+}
